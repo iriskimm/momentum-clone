a99046b9093b0254866173e7cb786adc06cbfb01
Iris Kim <iriskim@Iriss-MacBook-Pro.local>
2022-12-26 15:38:34 -0500

    add quotes

---
+++ js/quotes.js
@@ -0,0 +1,10 @@
+import data from '../quotes.json' assert { type: 'json' };
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const quotes = data.filter(quote => quote.Quote.length < 100 && quote.Author.length < 25);
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.Quote;
+author.innerText = "\n" + todaysQuote.Author;
