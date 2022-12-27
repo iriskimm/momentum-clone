7941cd7e444aac76f79caa78a9a108f84b5d38a2
Iris Kim <iriskim@Iriss-MacBook-Pro.local>
2022-12-27 15:41:50 -0500

    fetch weather info

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,15 @@ import '/js/config.js'
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in ", lat, lon);
-// const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+            city.innerText = data.name;
+        });
 }
 
 
